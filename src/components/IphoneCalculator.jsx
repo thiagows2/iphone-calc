52ed9ad21f49b30d62832c510220838d55e0af50
Thiago Marques <thiagomarques408@gmail.com>
2023-01-29 11:19:42 -0300

    feat: cria botões

--- src/components/IphoneCalculator.jsx
+++ src/components/IphoneCalculator.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
-import { Container, CalculatorContainer } from '../themes/IphoneCalculator'
+import { Container, CalculatorContainer, Button } from '../themes/IphoneCalculator'
+
+export const colors = {
+    BLACK: '#333333',
+    GRAY: '#A5A5A5',
+    ORANGE: '#F1A33B'
+  }
 
 export const IphoneCalculator = () => {
   return <Container>
     <CalculatorContainer>
-
+      <Button background={colors.GRAY}/>
+      <Button background={colors.GRAY}/>
+      <Button background={colors.GRAY}/>
+      <Button background={colors.ORANGE}/>
+      <Button />
+      <Button />
+      <Button />
+      <Button background={colors.ORANGE}/>
+      <Button />
+      <Button />
+      <Button />
+      <Button background={colors.ORANGE}/>
+      <Button />
+      <Button />
+      <Button />
+      <Button background={colors.ORANGE}/>
+      <Button width="190px" />
+      <Button />
+      <Button background={colors.ORANGE}/>
     </CalculatorContainer>
   </Container>
 }
--- src/themes/IphoneCalculator.js
+++ src/themes/IphoneCalculator.js
@@ -1,12 +1,28 @@
 import styled from 'styled-components';
+import { colors } from "../components/IphoneCalculator.jsx";
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
+  margin-top: 80px;
 `
 
 export const CalculatorContainer = styled.div`
   width: 428px;
   height: 683px;
   background-color: black;
+  border-radius: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 14px;
+  align-content: flex-end;
+  padding-bottom: 40px;
+`
+
+export const Button = styled.div`
+  width: ${({ width }) => width || '88px' };
+  height: 88px;
+  background: ${({ background }) => background || colors.BLACK };
+  border-radius: 352px;
 `
